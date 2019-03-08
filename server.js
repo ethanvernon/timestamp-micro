0090bceda3bbb8a4e483919cd9b8f558f2a1f07d
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-08 19:58:15 +0800

    change port number

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.get('/api/timestamp/:date_string?', function(req, res) {
 
 
 // listen for requests :)
-var listener = app.listen('process.env.PORT', function () {
+const portNumber = process.env.PORT || 8000;
+var listener = app.listen(portNumber, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
